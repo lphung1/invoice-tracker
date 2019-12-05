d3dbd7e9a383abc56c37624c54e96772b756bd6b
lphung1 <loi_phung81@yahoo.com>
2019-12-05 15:29:07 -0500

    updated with suraj code

--- angular-invoice-tracker/src/app/invoicelist/invoicelist.component.ts
+++ angular-invoice-tracker/src/app/invoicelist/invoicelist.component.ts
@@ -10,19 +10,19 @@ export class InvoicelistComponent implements OnInit {
 
   invocies: Invoice[] = [
     // tslint:disable-next-line: max-line-length
-    { invoiceId: 1, customer: 'Dr Nice', genDate: new Date('2019-12-01').toLocaleDateString(), amount: 2500, dueDate: new Date('2019-12-16').toLocaleDateString(), status: 'unPaid' },
+    { invoiceId: 1, customer: 'Dr Nice', genDate: new Date('2019-12-01'), amount: 2500, dueDate: new Date('2019-12-16'), status: 'unPaid' },
     // tslint:disable-next-line: max-line-length
-    { invoiceId: 2, customer: 'Dr New', genDate: new Date('2019-12-01').toLocaleDateString(), amount: 2500, dueDate: new Date('2019-12-16').toLocaleDateString(), status: 'unPaid' },
+    { invoiceId: 2, customer: 'Dr New', genDate: new Date('2019-12-01'), amount: 2500, dueDate: new Date('2019-12-16'), status: 'unPaid' },
     // tslint:disable-next-line: max-line-length
-    { invoiceId: 3, customer: 'Dr Blue', genDate: new Date('2019-12-01').toLocaleDateString(), amount: 2500, dueDate: new Date('2019-12-16').toLocaleDateString(), status: 'unPaid' },
+    { invoiceId: 3, customer: 'Dr Blue', genDate: new Date('2019-12-01'), amount: 2500, dueDate: new Date('2019-12-16'), status: 'unPaid' },
     // tslint:disable-next-line: max-line-length
-    { invoiceId: 4, customer: 'Dr Red', genDate: new Date('2019-12-01').toLocaleDateString(), amount: 2500, dueDate: new Date('2019-12-16').toLocaleDateString(), status: 'unPaid' },
+    { invoiceId: 4, customer: 'Dr Red', genDate: new Date('2019-12-01'), amount: 2500, dueDate: new Date('2019-12-16'), status: 'unPaid' },
     // tslint:disable-next-line: max-line-length
-    { invoiceId: 5, customer: 'Dr No', genDate: new Date('2019-12-01').toLocaleDateString(), amount: 2500, dueDate: new Date('2019-12-16').toLocaleDateString(), status: 'unPaid' },
+    { invoiceId: 5, customer: 'Dr No', genDate: new Date('2019-12-01'), amount: 2500, dueDate: new Date('2019-12-16'), status: 'unPaid' },
     // tslint:disable-next-line: max-line-length
-    { invoiceId: 6, customer: 'Dr Dan', genDate: new Date('2019-12-01').toLocaleDateString(), amount: 2500, dueDate: new Date('2019-12-16').toLocaleDateString(), status: 'unPaid' },
+    { invoiceId: 6, customer: 'Dr Dan', genDate: new Date('2019-12-01'), amount: 2500, dueDate: new Date('2019-12-16'), status: 'unPaid' },
     // tslint:disable-next-line: max-line-length
-    { invoiceId: 7, customer: 'Dr Pickles', genDate: new Date('2019-12-01').toLocaleDateString(), amount: 2500, dueDate: new Date('2019-12-16').toLocaleDateString(), status: 'unPaid' }, 
+    { invoiceId: 7, customer: 'Dr Pickles', genDate: new Date('2019-12-01'), amount: 2500, dueDate: new Date('2019-12-16'), status: 'unPaid' }, 
   ];
 
   constructor() { }
