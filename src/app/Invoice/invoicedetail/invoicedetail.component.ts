29331b8da625d2459eb2a08ca63f4657a38b6de3
lphung1 <loi_phung81@yahoo.com>
2019-12-12 14:16:16 -0500

    fixed pdf error, need to fix formatting

--- src/app/Invoice/invoicedetail/invoicedetail.component.ts
+++ src/app/Invoice/invoicedetail/invoicedetail.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { InvoiceLine } from './../../Models/InvoiceLine';
 import { PDFJS } from 'pdfjs-dist';
 import * as jspdf from 'jspdf';
-import html2canvas from 'html2canvas'
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-invoicedetail',
@@ -13,32 +13,38 @@ import html2canvas from 'html2canvas'
 })
 export class InvoicedetailComponent implements OnInit {
 
- @Input() invoice: Invoice;
- 
+  @Input() invoice: Invoice;
+
   constructor(private service: InvoiceDetailService) { }
 
   invoicelines: InvoiceLine[] = [
-    { invoiceId: 1, invoiceLineId: 1, description: 'plumbing' , price: 2500,quantity : 1},
-    { invoiceId: 1, invoiceLineId: 2, description: 'electrical', price: 500,quantity : 1},
-    { invoiceId: 1, invoiceLineId: 3, description: 'regrigeration',price: 52500,quantity :1},
-    
+    { invoiceId: 1, invoiceLineId: 1, description: 'plumbing', price: 2500, quantity: 1 },
+    { invoiceId: 1, invoiceLineId: 2, description: 'electrical', price: 500, quantity: 1 },
+    { invoiceId: 1, invoiceLineId: 3, description: 'regrigeration', price: 52500, quantity: 1 },
 
-  ]
+  ];
 
 
   inv = this.service.getInvoice();
 
-  onBckClick()
-  {
-    window.history.back();
-  }
+
 
   getPDF() {
-    //const elementToPrint = document.getElementById('pdfContent'); 
-    //const pdf = new jsPDF('p', 'pt', 'a4');
-    //pdf.addHTML(elementToPrint, () => {
-    //pdf.save('web.pdf');
 
+    var data = document.getElementById('pdfContent');
+    html2canvas(data).then(canvas => {
+      // Few necessary setting options
+      var imgWidth = 208;
+      var pageHeight = 295;
+      var imgHeight = canvas.height * imgWidth / canvas.width;
+      var heightLeft = imgHeight;
+
+      const contentDataURL = canvas.toDataURL('image/png');
+      let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF
+      var position = 0;
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+      pdf.save('Invoice.pdf'); // Generated PDF
+    });
 
 
   }
