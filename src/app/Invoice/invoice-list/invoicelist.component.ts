12b4b318e1572242ad1b81bee18d0d454534ae51
Suraj <suraj.sowkar@gmail.com>
2019-12-12 14:59:20 -0500

    editted invoice list

--- src/app/Invoice/invoice-list/invoicelist.component.ts
+++ src/app/Invoice/invoice-list/invoicelist.component.ts
@@ -14,21 +14,21 @@ import { Router } from '@angular/router';
 export class InvoicelistComponent implements OnInit {
   [x: string]: any;
 
-   invocies: Invoice[] = [
+  invocies: Invoice[] = [
     // tslint:disable-next-line: max-line-length
-    { invoiceId: 1, customer: 'Dr Nice', genDate: new Date('2019-12-01'), amount: 2500, dueDate: new Date('2019-12-16'), status: 'unPaid', invoiceLines: new InvoiceLine() },
+    { invoiceId: 1, customer: 'Dr Nice', genDate: new Date('2019-12-01'), amount: 2500, dueDate: new Date('2019-12-16'), status: 'unPaid', invoiceLines: [] },
     // tslint:disable-next-line: max-line-length
-    { invoiceId: 2, customer: 'Dr New', genDate: new Date('2019-12-01'), amount: 2500, dueDate: new Date('2019-12-16'), status: 'unPaid' , invoiceLines: new InvoiceLine()},
+    { invoiceId: 2, customer: 'Dr New', genDate: new Date('2019-12-01'), amount: 2500, dueDate: new Date('2019-12-16'), status: 'unPaid' , invoiceLines: []},
     // tslint:disable-next-line: max-line-length
-    { invoiceId: 3, customer: 'Dr Blue', genDate: new Date('2019-12-01'), amount: 2500, dueDate: new Date('2019-12-16'), status: 'unPaid', invoiceLines: new InvoiceLine() },
+    { invoiceId: 3, customer: 'Dr Blue', genDate: new Date('2019-12-01'), amount: 2500, dueDate: new Date('2019-12-16'), status: 'unPaid', invoiceLines: [] },
     // tslint:disable-next-line: max-line-length
-    { invoiceId: 4, customer: 'Dr Red', genDate: new Date('2019-12-01'), amount: 2500, dueDate: new Date('2019-12-16'), status: 'unPaid' , invoiceLines: new InvoiceLine()},
+    { invoiceId: 4, customer: 'Dr Red', genDate: new Date('2019-12-01'), amount: 2500, dueDate: new Date('2019-12-16'), status: 'unPaid' , invoiceLines: []},
     // tslint:disable-next-line: max-line-length
-    { invoiceId: 5, customer: 'Dr No', genDate: new Date('2019-12-01'), amount: 2500, dueDate: new Date('2019-12-16'), status: 'unPaid' , invoiceLines: new InvoiceLine()},
+    { invoiceId: 5, customer: 'Dr No', genDate: new Date('2019-12-01'), amount: 2500, dueDate: new Date('2019-12-16'), status: 'unPaid' , invoiceLines: []},
     // tslint:disable-next-line: max-line-length
-    { invoiceId: 6, customer: 'Dr Dan', genDate: new Date('2019-12-01'), amount: 2500, dueDate: new Date('2019-12-16'), status: 'unPaid' , invoiceLines: new InvoiceLine()},
+    { invoiceId: 6, customer: 'Dr Dan', genDate: new Date('2019-12-01'), amount: 2500, dueDate: new Date('2019-12-16'), status: 'unPaid' , invoiceLines: []},
     // tslint:disable-next-line: max-line-length
-    { invoiceId: 7, customer: 'Dr Pickles', genDate: new Date('2019-12-01'), amount: 2500, dueDate: new Date('2019-12-16'), status: 'unPaid' , invoiceLines: new InvoiceLine()}, 
+    { invoiceId: 7, customer: 'Dr Pickles', genDate: new Date('2019-12-01'), amount: 2500, dueDate: new Date('2019-12-16'), status: 'unPaid' , invoiceLines: []}, 
   ];
 
   selectedInvoice: Invoice;
