48cd377b3bc0e30d4960a902faa4e427c6545f9a
Suraj <suraj.sowkar@gmail.com>
2019-12-19 14:01:06 -0500

    post method changes

--- src/app/customer/new-customer/new-customer.component.ts
+++ src/app/customer/new-customer/new-customer.component.ts
@@ -1,7 +1,7 @@
 import { CustomerService } from './../../customer.service';
 import { Customer } from 'src/app/Models/Customer';
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-new-customer',
@@ -18,11 +18,22 @@ export class NewCustomerComponent implements OnInit {
   OnSubmit() {
     this.service.Save(this.customer).subscribe(resp => {
       console.log(resp );
-      if( resp != null) {
-        window.alert('Customer Added!');
-        this.customer = new Customer();
+      window.alert('Customer Added!');
+      this.customer = new Customer();
+      },
+    (err: HttpErrorResponse) => {
+      if (err.error instanceof Error) {
+        // A client-side or network error occurred.
+        console.log('An error occurred:', err.error.message);
+        window.alert('Customer not Added!');
+      } else {
+        // Backend returns unsuccessful response codes such as 404, 500 etc.
+        console.log('Backend returned status code: ', err.status);
+        console.log('Response body:', err.error);
+        window.alert('Customer not Added!');
       }
-    });
+    }
+    );
   }
 
 }
